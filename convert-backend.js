08cde040f4376109f970d8875792e08bebd99ba0
Maslin Farrell <maslinfar@gmail.com>
2023-02-17 16:52:51 -0800

    Setup a queue system for multiple jobs
    Created a job queue to handle multiple users. All that's remaining is to figure out how to handle downloads, without headers closing.

--- convert-backend.js
+++ convert-backend.js
@@ -1,177 +1,64 @@
-require('dotenv').config();
-
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const fs = require('fs-extra');
-const sharp = require('sharp');
-const AdmZip = require('adm-zip');
+const crypto = require('crypto');
+
+//Import the job processor class
+const JobProcessor = require('./sharp-processor.js');
+
 const app = express();
 
-//Initializing fileUpload
+// Initializing fileUpload
 app.use(fileUpload());
 
 
-/**
- * Sets up required directories
- * if processed/upload dir exists -> make sure it's empty
- * if processed/upload dir DNE -> create the directories
- */
-fs.emptyDir('./processed');
-fs.emptyDir('./uploaded');
-fs.emptyDir('./zipped');
+// Create the Jobs directory where each processing job will be located
+fs.emptyDir('./Jobs');
+
+// TODO: Remove qc folder and set it up as a download
+fs.emptyDir('./qc');
 
 // Start listening for requests on assigned port
 app.listen(process.env.PORT, () => {
     console.log(`Listening on PORT ${process.env.PORT}`);
 });
 
+// This is where we will store pending jobs
+var JOB_QUEUE=[];
+
 // Upload endpoint
 app.post('/upload', async function(req, res) {
-    console.log(req);
-    // Check to see if files were uploaded
-    if (!req.files || Object.keys(req.files).length === 0) {
+    
+    // Check to see if files were uploaded in the request body
+    if (!req.files || Object.keys(req.files).length === 0)
         return res.status(400).send('No files were uploaded.');
-    }
     
-    // Because JS is special imgToUpload when multiple files are uploaded, it's an array. When a single image is uploaded it is not.
-    // To support processing multiple images we check if the length is undefined. If a single image is uploaded store it to an array and process it
-    // if not change the imgs variable from an empty array to the imgToUpload array
-    let imgs = [];
+    // Imgs array used for storing the location of the uploaded images
+    let imgsArray = [];
+
+    // If only one image was uploaded it is simply an image object so we push it to an array
     if(req.files.imgToUpload.length === undefined)
-        imgs.push(req.files.imgToUpload);
+        imgsArray.push(req.files.imgToUpload);
+    // Since multiple images automatically get put into an array (by either javascript or file-upload) we set the imgsArray var to the array from the request body
     else
-        imgs = req.files.imgToUpload;
-    
-    setupImageProcessing(req, res, imgs);
-    
-    // Zip all of the files in the processed directory and send the download
-    function zipProcessed() {
-        const zip = new AdmZip();
-        //Give a timestamp to the zipped folder
-        var timestamp = new Date().getTime();
-        const outputFile = `./zipped/Processed_Images_${Math.floor(timestamp / 1000)}.zip`;
-        zip.addLocalFolder("./processed");
-        zip.writeZip(outputFile);
-        res.download(outputFile);
+        imgsArray = req.files.imgToUpload;
 
-        //Clean out processed and zipped directories
-        fs.emptyDir('./processed');
-        fs.emptyDir('./zipped');
-    }
-
-    //If imgs array contains one image we need to set the appropriate amount of time to wait until the image has processed
-    if (imgs.length === 1) {
-        setTimeout(zipProcessed, env.MOD_Batch);
-    } else {
-        //delay the start of zip operation by length of imgs * modifier (num of ms it takes on avg to process an img)
-        setTimeout(zipProcessed, imgs.length*process.env.MOD_Single);    
-    }
-    
+    // Creating a UUID for the current job
+    let uuid = crypto.randomUUID()
+    // Create a new job with the UUID, request and imgsArray
+    let job = new JobProcessor(uuid, req, imgsArray);
+    // Push the new job to the JOB_QUEUE
+    JOB_QUEUE.push(job);
+    // Since we have a job run the checkJobQueue
+    checkJobQueue();
 });
 
-function setupImageProcessing(req, res, imgs) {
-    imgs.forEach(uploadedImg => {
-        
-        /**
-         * Setting correct file name
-         * - If resize was requested use the original file name which includes the original file extension
-         * - If convert was requested use the original file name and change the extension to the converted ext (ie: original ext is .png converted ext is .webp)
-         */
-        let convertedFileName;
-
-        // If resize was requested or converted file ext was left empty use the original file extension
-        if(req.body.fileExt === "empty"){
-            convertedFileName = uploadedImg.name;
-        }else{
-            // Split the uploaded file name into [file name, file ext] get the name and append the converted extension to it
-            convertedFileName = uploadedImg.name.split('.')[0] + '.' + req.body.fileExt;
-        }
-        
-        let uploadedFilePath = "./uploaded/" + uploadedImg.name; // get the uploaded image and it's path
-        let processedFilePath = "./processed/" + convertedFileName; // where the processed file will be stored and it's name
-
-        //Use the mv() method to place the file in the "/uploaded" dir on the server
-        uploadedImg.mv(uploadedFilePath, function(err) {
-            if (err)
-                return res.status(500).send(err);
-            
-            //Run the convert function if it was selected
-            if(req.body.function === 'convert'){
-                
-                // If the "Convert the image to" dropdown was the same file extension as the uploaded img 
-                // DO NOT perform the conversion just return the uploaded file
-                if(req.body.function === 'convert' && req.body.fileExt === uploadedImg.name.split('.')[1]){
-                    fs.moveSync(uploadedFilePath, processedFilePath);
-                    res.send('No conversion needed check processed');
-
-                //Otherwise perform the conversion
-                }else{
-                    convert(req, res, uploadedFilePath, processedFilePath);
-                }    
-            }
-            
-            //Call the resize function
-            if(req.body.function === 'resize')
-                resize(req, res, uploadedFilePath, processedFilePath);
-        });
-    });
-}
-
-
-
-/**
- * Convert the image using Sharp,  Jpeg conversion is set to highest quality jpeg, PNG by default is lossless, Webp is compressed and uses highest CPU setting
- * @param {*} req contains the request from the frontend. Used for determining what to convert the uploaded file to
- * @param {*} res 
- * @param {*} uploadedFilePath contains the location of the uploaded file
- * @param {*} processedFilePath where the processed file should be stored
- */
-function convert(req, res, uploadedFilePath, processedFilePath) {
-    if(req.body.fileExt === 'jpg'){
-        sharp(uploadedFilePath)
-            .jpeg({
-                quality: 100,
-                chromaSubsampling: '4:4:4'
-            })
-            .toFile(processedFilePath)
-            .then(() => {
-                fs.removeSync(uploadedFilePath);
-            });
-
-    }else if(req.body.fileExt === 'png'){
-        sharp(uploadedFilePath)
-            .png()
-            .toFile(processedFilePath)
-            .then(() => {
-                fs.removeSync(uploadedFilePath);
-            })
-
-    }else if(req.body.fileExt === 'webp'){
-        sharp(uploadedFilePath)
-            .webp({effort: 0})
-            .toFile(processedFilePath)
-            .then(() => {
-                fs.removeSync(uploadedFilePath);
-            })
-    }else{
-        res.status(400).send('No conversion performed options not selected');
+function checkJobQueue() {
+    // If the JOB_QUEUE contains a job
+    if (JOB_QUEUE.length !== 0) {
+        // Get the job
+        let fetchJob = JOB_QUEUE.pop();
+        // Run the job
+        fetchJob.runJob();
     }
-}
-
-/**
- * Resize the image using Sharp,  Resizing is done using the default fill mode, process avoids any weird stretching/scaling problems
- * @param {*} req contains the request from the frontend. Used for determining what resolution to resize the image to
- * @param {*} res 
- * @param {*} uploadedFilePath contains the location of the uploaded file
- * @param {*} processedFilePath where the processed file should be stored
- */
-function resize(req, res, uploadedFilePath, processedFilePath) {
-    sharp(uploadedFilePath)
-        //Resize input file and write it to export path
-        .resize(parseInt(req.body.width), parseInt(req.body.height))
-        .toFile(processedFilePath)
-        .then(() => {
-            //remove the uploaded file from uploaded folder 
-            fs.removeSync(uploadedFilePath)
-        })
 }
---
+++ sharp-processor.js
@@ -0,0 +1,147 @@
+require('dotenv').config();
+const fs = require('fs-extra');
+const sharp  = require('sharp');
+const AdmZip = require('adm-zip');
+
+class Job{
+    constructor(uuid, req, imgs){
+        this.uuid = uuid;
+        this.req = req;
+        this.imgs = imgs;
+    }
+
+    runJob(){
+        let user = this.uuid;
+        let request = this.req;
+        let imgArray = this.imgs;
+
+        //Create a folder using the UUID, setup processed, uploaded, and zipped folders
+        fs.emptyDir(`./Jobs/${user}`)
+        fs.emptyDir(`./Jobs/${user}/processed`);
+        fs.emptyDir(`./Jobs/${user}/uploaded`);
+        fs.emptyDir(`./Jobs/${user}/zipped`);
+
+        /**
+         * Process the image(s) that were uploaded using the imgArray
+         * - First define our upload and processing folders
+         * - Store the uploaded images into the uploaded folder
+         * - Check if were are converting or resizing
+         *      - If we are converting set the output file name (using the uploaded file name) and extension (using the requested file extension)
+         *          - If the image extension is the same as the original no need to convert it simply move it to the processed folder
+         *        Lastly convert the image using sharp
+         *      - If we are resizing use the original file name and extension, resize the image(s)
+         */
+        function processImages() {
+            imgArray.forEach(uploadedImg => {
+                let uploadedFilePath = `./Jobs/${user}/uploaded/` + uploadedImg.name;
+                let processedFilePath = `./Jobs/${user}/processed/`;
+                uploadedImg.mv(uploadedFilePath, function(){
+                    if(request.body.function === 'convert'){
+                        if(request.body.function === 'convert' && request.body.fileExt === uploadedImg.name.split('.'[1])){
+                            fs.moveSync(uploadedFilePath, processedFilePath + uploadedImg.name);
+                        }else{
+                            // Split the uploaded file name into [file name, file ext] get the name and append the converted extension to it
+                            let convertedFileName = uploadedImg.name.split('.')[0] + '.' + request.body.fileExt;
+                            processedFilePath += convertedFileName;
+                            convert(request, uploadedFilePath, processedFilePath);
+                        }
+                    }
+                    if(request.body.function === 'resize')
+                        resize(request, uploadedFilePath, processedFilePath + uploadedImg.name);
+                });
+            });
+        }
+        //Wait ___ ms for the folders to be created 
+        // TODO: Adjust the timing and someday when I learn JS switch it out to callbacks or async's
+        setTimeout(processImages, 80);
+
+        /**
+         * Convert the image using Sharp,  Jpeg conversion is set to highest quality jpeg, PNG by default is lossless, Webp is compressed and uses highest CPU setting
+         * @param {*} req contains the request from the frontend. Used for determining what to convert the uploaded file to
+         * @param {*} uploadedFilePath contains the location of the uploaded file
+         * @param {*} processedFilePath where the processed file should be stored
+         */
+        function convert(req, uploadedFilePath, processedFilePath) {
+            if(req.body.fileExt === 'jpg'){
+                sharp(uploadedFilePath)
+                    .jpeg({
+                        quality: 100,
+                        chromaSubsampling: '4:4:4'
+                    })
+                    .toFile(processedFilePath)
+                    .then(() => {
+                        fs.removeSync(uploadedFilePath);
+                    });
+        
+            }else if(req.body.fileExt === 'png'){
+                sharp(uploadedFilePath)
+                    .png()
+                    .toFile(processedFilePath)
+                    .then(() => {
+                        fs.removeSync(uploadedFilePath);
+                    })
+        
+            }else if(req.body.fileExt === 'webp'){
+                sharp(uploadedFilePath)
+                    .webp({effort: 0})
+                    .toFile(processedFilePath)
+                    .then(() => {
+                        fs.removeSync(uploadedFilePath);
+                    })
+            }
+        }
+
+        /**
+         * Resize the image using Sharp,  Resizing is done using the default fill mode, process avoids any weird stretching/scaling problems
+         * @param {*} req contains the request from the frontend. Used for determining what resolution to resize the image to 
+         * @param {*} uploadedFilePath contains the location of the uploaded file
+         * @param {*} processedFilePath where the processed file should be stored
+         */
+        function resize(req, uploadedFilePath, processedFilePath) {
+            sharp(uploadedFilePath)
+                //Resize input file and write it to export path
+                .resize(parseInt(req.body.width), parseInt(req.body.height))
+                .toFile(processedFilePath)
+                .then(() => {
+                    //remove the uploaded file from uploaded folder 
+                    fs.removeSync(uploadedFilePath)
+                })
+        }
+
+        function zipProcessed(){
+            // initialize AdamZip
+            const zip = new AdmZip();
+
+            //Give a timestamp to the zipped folder
+            var timestamp = new Date().getTime();
+            
+            // TODO: Set this up as a download IF a single image is uploaded no need to zip it up just send it out
+            const outputFile = `./qc/Processed_Images_${Math.floor(timestamp)}.zip`;
+            
+            //Add the processed image(s) to the zip
+            zip.addLocalFolder(`./Jobs/${user}/processed`);
+            zip.writeZip(outputFile);
+            //res.download(outputFile);
+
+            //Clean out processed and zipped directories
+            fs.emptyDir(`./Jobs/${user}/processed`);
+            fs.emptyDir(`./Jobs/${user}/zipped`);
+        }
+
+        //If imgs array contains one image we need to set the appropriate amount of time to wait until it has been zipped up
+        if (imgArray.length === 1) {
+            setTimeout(zipProcessed, process.env.MOD_SINGLE);
+        } else {
+            //delay the start of zip operation by length of imgArray * modifier (num of ms it takes on avg to process an img)
+            setTimeout(zipProcessed, imgArray.length*process.env.MOD_BATCH);    
+        }
+
+        // Removes the UUID job folder after ___ seconds ensures enough time to download the zip file or image
+        function cleanUP() {
+            fs.rmSync(`./Jobs/${user}`, {recursive: true, force: true});
+        }
+        setTimeout(cleanUP, 15000);
+    }
+}
+
+module.exports = Job
